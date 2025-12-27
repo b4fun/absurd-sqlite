36c973ff9f29dbcb337c45cc3ae6261e7e7cf72c
hbc <me@hbc.rocks>
2025-12-27 14:32:56 -0800

    feat: poc of spawning shell process

--- standalone/src-tauri/src/lib.rs
+++ standalone/src-tauri/src/lib.rs
@@ -1,3 +1,10 @@
+use log;
+use tauri::async_runtime;
+
+use crate::worker::spawn_worker;
+
+mod worker;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,8 +14,26 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(tauri_plugin_log::log::LevelFilter::Info)
+                .target(tauri_plugin_log::Target::new(
+                    tauri_plugin_log::TargetKind::Stderr,
+                ))
+                .build(),
+        )
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let app_handle = app.handle().clone();
+
+            async_runtime::spawn(async move { spawn_worker(&app_handle).await });
+
+            log::info!("setup");
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
