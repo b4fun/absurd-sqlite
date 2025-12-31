de787189cd369f064daa0f3e9406a8ea5f7ab635
hbc <me@hbc.rocks>
2025-12-31 11:35:09 -0800

    fix: update log

--- standalone/src-tauri/build.rs
+++ standalone/src-tauri/build.rs
@@ -51,7 +51,7 @@ fn prepare_sqlite_extension() {
     let dest = bin_dir.join("absurd-extension");
     fs::copy(&extension_path, &dest).expect("copy SQLite extension into resources");
     println!(
-        "cargo:warning=Bundling absurd_sqlite_extension from {}",
+        "cargo:info=Copying absurd_sqlite_extension from {}",
         extension_path.display()
     );
 }
